e7555535e3a40739702622053ec3b61147fb5b4c
Todd Coulson <tcoulson@gmail.com>
2017-03-01 21:37:08 -0500

    Add files via upload
    adding the latest files for production upload

--- server.js
+++ server.js
@@ -4,29 +4,7 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 8080;
 
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var morgan = require('morgan');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var flash = require('connect-flash');
-
-
-//var configDB = require('./config/database.js');
-//mongoose.connect(configDB.url);
-//require('./config/passport')(passport);
-
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(session({secret: 'anystringoftext',
-				 saveUninitialized: true,
-				 resave: true}));
-
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use(express.static(__dirname + '/app'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
@@ -43,7 +21,7 @@ app.use('/bootstrap-photo-gallery',  express.static(__dirname + '/bootstrap-phot
 // 	console.log(req.session);
 // });
 
-require('./app/routes.js')(app, passport);
+require('./app/routes.js')(app);
 
 app.listen(port);
 console.log('Server running on port: ' + port);
